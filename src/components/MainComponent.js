958c1fca53cd9376303a160ea4f51d27b3e64c20
Lindsay P <lpiethe@gmail.com>
2021-01-12 06:58:19 -0800

    Google Analytic pagetracking attempt

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,10 +7,21 @@ import Projects from './ProjectsComponent';
 import Home from './HomePageComponent';
 import Contact from './ContactComponent';
 import NavBar from './NavBarComponent';
+import ReactGA from 'react-ga';
+import { createBrowserHistory } from 'history';
 
 class Main extends Component {
     render() {
 
+        const history = createBrowserHistory();
+
+// Initialize google analytics page view tracking
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
+        
+        
         const HomePage = () => {
             return (
                 <Home />
@@ -18,7 +29,7 @@ class Main extends Component {
         }
 
         return (
-            <Router>
+            <Router history={history}>
 
             <div>
                 <NavBar />
